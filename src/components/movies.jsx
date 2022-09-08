c60ba7535540afbb16af90f273f0dc9edd4b89f0
Sergio Nieto <snieto10@gmail.com>
2022-09-07 21:17:53 -0400

    delete button

---
+++ src/components/movies.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { getMovies } from "../fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>Showing {this.totalMovies()} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tbody></tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  totalMovies() {
+    return this.state.movies.length;
+  }
+}
+
+export default Movies;
