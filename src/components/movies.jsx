35f92d468441d730549565176f38bc6c21a463bb
Sergio Nieto <snieto10@gmail.com>
2022-09-09 13:42:57 -0400

    section 4 setp 15

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,8 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres " }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = (movie) => {
@@ -36,16 +37,27 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    console.log(genre);
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, genres, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      genres,
+      selectedGenre,
+      movies: allMovies,
+    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -53,8 +65,7 @@ class Movies extends Component {
           <ListGroup
             items={genres}
             onItemSelect={this.handleGenreSelect}
-            textProperty="name"
-            valueProperty="_id"
+            selectedItem={this.state.selectedGenre}
           />
         </div>
         <div className="col">
@@ -99,7 +110,7 @@ class Movies extends Component {
             <tbody></tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             onPageChange={this.handlePageChange}
             currentPage={currentPage}
